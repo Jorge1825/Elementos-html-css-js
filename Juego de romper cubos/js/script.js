59a4990485fe4fef76f0ddb7a568c92c77dd5653
Jorge Luis <jroatoro362@gmail.com>
2022-09-09 20:06:35 -0500

    efecto humo y nuevo menu

--- Juego de romper cubos/js/script.js
+++ Juego de romper cubos/js/script.js
@@ -54,4 +54,116 @@ const velocidadMinimaPuntero = 60;
 const amortiguarGolpe = 0.1;
 
 // El tablero recibe sombras y es la posición Z negativa más lejana de las entidades.
+const sombraEnTablero = -400;
+const colorSombra = "#262e36"
 
+
+// Cuanta resistencia aerodinámica se aplica a los objetos estándar
+const resistenciaAerodinamica = 0.022;
+const gravedad = 0.3;
+
+
+//Configuracion de chispa 
+const colorChispa = 'rgba(170,221,255,.9)';
+const tamañoChispa = 2.2;
+const resistenciaAerodinamicaChispa = 0.1;
+
+// Seguimiento de las posiciones del puntero para mostrar el rastro
+const rastroPunteroColor = 'rgba(170,221,255,.9)';
+const rastroPunteroTamaño = 7;
+const vidaUtiPuntero = 120;
+const puntosPuntero = [];
+
+
+// Tamaño de los objetivos en el juego. Esto afecta el tamaño renderizado y el área de impacto.
+
+const radioObjetivo = 40;
+const radioImpactoObjetivo = 50;
+const hacerColorPegasoObjetivo = objetivo => {
+    return 'rgb(170,221,255)';
+}
+
+
+//Tamaño de los fragmentos del objetivo
+const radioFragmento = radioObjetivo/3;
+
+
+//---Elemento canvas necesario para el juego y la interaccion
+const canvas = document.getElementById('canvas');
+
+
+//Configuracion 3D de la camara
+//Efectos para la perspectiva de la camara
+const distanciaCamara = 900;
+
+
+//No afecta la perpectiva
+const alturaCamara = 1;
+
+
+// Los objetos que se acerquen demasiado a la cámara se desvanecerán a transparentes en este rango.
+const inicioDesvanecimientoCamaraZ = 0.45 * distanciaCamara;
+const finDesvanecimientoCamaraZ = 0.65 * distanciaCamara;
+const rangoDesvanecimientoCamaraZ = finDesvanecimientoCamaraZ - inicioDesvanecimientoCamaraZ;
+
+
+//Globales usados ​​para acumular todos los vértices/polígonos en cada marco para renderizarlos en un solo llamado de función.
+
+const todosVortices = [];
+const todosPoligonos = [];
+const todasSombrasVetices = [];
+const todasSombrasPoligonos = [];
+
+
+
+
+
+
+// estado.js
+// ============================================================================
+// ============================================================================
+
+///////////
+// Enumeraciones //
+///////////
+
+
+
+//Modos de juego
+const MODO_JUEGO_NORMAL = Symbol('MODO_JUEGO_NORMAL');
+const MODO_JUEGO_CASUAL = Symbol('MODO_JUEGO_CASUAL');
+
+
+//Menus disponibles
+const MENU_PRINCIPAL = Symbol('MENU_PRINCIPAL');
+const MENU_PAUSA = Symbol('MENU_PAUSA');
+const MENU_PUNTUACION = Symbol('MENU_PUNTUACION');
+
+
+
+
+///////////////////
+// Estado global //
+///////////////////
+
+const estado = {
+    juego: {
+        modo: MODO_JUEGO_NORMAL,
+
+        tiempo: 0,
+
+        puntos: 0,
+
+        contarCubos: 0
+    },
+    menus:{
+        activo: MENU_PRINCIPAL
+    }
+};
+
+
+
+
+/////////////////////////////////
+// estado global de selectores //
+/////////////////////////////////
